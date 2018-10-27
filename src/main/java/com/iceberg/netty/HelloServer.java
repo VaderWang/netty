858cfec8674ae10f37bc9923dcd05edc2e79f809
Vader Wang <2953381201@qq.com>
2018-10-27 09:53:59 +0800

    hello

--- src/main/java/com/iceberg/netty/HelloServer.java
+++ src/main/java/com/iceberg/netty/HelloServer.java
@@ -1,13 +1,29 @@
 package com.iceberg.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class HelloServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
+        //主线程组
         NioEventLoopGroup masterGroup = new NioEventLoopGroup();
 
+        //从线程组
         NioEventLoopGroup slaveGroup = new NioEventLoopGroup();
+
+        //启动类
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        //简单设置
+        serverBootstrap.group(masterGroup, slaveGroup).channel(NioServerSocketChannel.class).childHandler(null);
+
+        //启动server
+        ChannelFuture channelFuture = serverBootstrap.bind(8088).sync();
+
+        channelFuture.channel().closeFuture().sync();
 
 
     }
